f4596c2773563c17ab63ec2d83a6d11db57d08f5
nileshdhale <nilesh.rkr@gmail.com>
2022-10-02 13:40:29 +0530

    first commit

---
+++ Vespa/src/test/java/com/testcases/TestClass.java
@@ -0,0 +1,39 @@
+package com.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass {
+	@Test
+	public void TestOne() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shree\\Documents\\New folder\\chromedriver_win32 (2)\\chromedriver.exe");
+		WebDriver	driver= new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.print("Test One done");
+		
+		driver.quit();
+	}@Test
+	public void TestTwo() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shree\\Documents\\New folder\\chromedriver_win32 (2)\\chromedriver.exe");
+		WebDriver	driver= new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.print("Test Two done");
+		
+		driver.quit();
+		}
+	
+		@Test
+		public void TestThree() {
+			Assert.assertTrue(true);
+			System.out.print("Assertions passedd");
+			
+			
+			
+		}
+	
+	
+	
+
+}
